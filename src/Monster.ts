eed7da09a8c11fa9b7830f777ecbfad17665c68b
GabrielSilvaCosta <bob@>
2023-09-20 14:36:13 -0300

    requisito 9 feito

---
+++ src/Monster.ts
@@ -0,0 +1,38 @@
+import Fighter from './Fighter';
+import getRandomInt from './utils';
+
+class Monster implements Fighter {
+  private _lifePoints = 85;
+  private _strength = 63;
+  private _defense: number = getRandomInt(1, 10);
+
+  levelUp(): void {
+    this._defense += 5;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    if (this._lifePoints < 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+}
+
+export default Monster;
